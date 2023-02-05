cae76cd5b6772f85cc44ae071023cdebb3afaa5b
Ashish9431 <ashishshar99ma@gmail.com>
2023-02-05 21:04:20 +0530

    Nav

--- reliancedigital/src/Components/NavComponents/Hamburger.jsx
+++ reliancedigital/src/Components/NavComponents/Hamburger.jsx
@@ -14,6 +14,7 @@ import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons"
 import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 
+
 export const Hamburger = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = React.useRef()
